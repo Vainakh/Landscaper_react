48262f21fb1f0a3bb6bb4dccf2fa8bfda9f82a7a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-07-05 20:27:31 -0700

    Create app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 
 const port = Number(process.env.PORT || 4000); 
